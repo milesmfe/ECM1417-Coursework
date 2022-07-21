81c9acc6faf4cec7be372143f0c371849832c6ce
Miles Edwards <miles.mfe@gmail.com>
2022-07-21 10:50:35 +0100

    testing

--- tetris/index.php
+++ tetris/index.php
@@ -6,7 +6,7 @@
 	</head>
 	<body>
 		<ul>
-			<li name="home" style="float:left"><a href="index.php">Home</a></li>
+			<li name="home" style="float:left"><a class="current" href="index.php">Home</a></li>
 			<li name="tetris" style="float:right"><a href="tetris.php">Play Tetris</a></li>
 			<li name="leaderboard" style="float:right"><a href="leaderboard.php">Leaderboard</a></li>
 		</ul>

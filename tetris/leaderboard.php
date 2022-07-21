79109d57d678c0c892fbd7ba5ebed22c4a72ca84
Miles Edwards <miles.mfe@gmail.com>
2022-07-21 11:05:23 +0100

    coppied nav bar into other pages

---
+++ tetris/leaderboard.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Leaderboard</title>
+		<link rel="stylesheet" href = "css/main.css">
+	</head>
+	<body>
+		<ul>
+			<li name="home" style="float:left"><a href="index.php">Home</a></li>
+			<li name="tetris" style="float:right"><a href="tetris.php">Play Tetris</a></li>
+			<li name="leaderboard" style="float:right"><a class="current" href="leaderboard.php">Leaderboard</a></li>
+		</ul>
+	</body>
+</html>
---
+++ tetris/tetris.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Play Tetris</title>
+		<link rel="stylesheet" href = "css/main.css">
+	</head>
+	<body>
+		<ul>
+			<li name="home" style="float:left"><a href="index.php">Home</a></li>
+			<li name="tetris" style="float:right"><a class="current" href="tetris.php">Play Tetris</a></li>
+			<li name="leaderboard" style="float:right"><a href="leaderboard.php">Leaderboard</a></li>
+		</ul>
+	</body>
+</html>

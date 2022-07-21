82835c408e62c26794dafa84a8733c2c65679850
Miles Edwards <miles.mfe@gmail.com>
2022-07-21 09:56:15 +0100

    Setup workspace

---
+++ tetris/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Tetris Homepage</title>
+		</style href = "tetris/main.css">
+	</head>
+	<body>
+		<ul>
+			<li name="home" style="float:left"><a href="tetris/index.php">Home</a></li>
+			<li name="tetris" style="float:right"><a href="tetris/tetris.php">Play Tetris</a></li>
+			<li name="leaderboard" style="float:right"><a href="tetris/leaderboard.php">Leaderboard</a></li>
+		</ul>
+	</body>
+</html>

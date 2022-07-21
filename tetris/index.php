1daf7a59325bbee7227784b740784801ad58b4d3
Miles Edwards <miles.mfe@gmail.com>
2022-07-21 10:04:50 +0100

    Fixed style reference in index

--- tetris/index.php
+++ tetris/index.php
@@ -2,13 +2,13 @@
 <html>
 	<head>
 		<title>Tetris Homepage</title>
-		</style href = "tetris/main.css">
+		</style href = "css/main.css">
 	</head>
 	<body>
 		<ul>
-			<li name="home" style="float:left"><a href="tetris/index.php">Home</a></li>
-			<li name="tetris" style="float:right"><a href="tetris/tetris.php">Play Tetris</a></li>
-			<li name="leaderboard" style="float:right"><a href="tetris/leaderboard.php">Leaderboard</a></li>
+			<li name="home" style="float:left"><a href="/index.php">Home</a></li>
+			<li name="tetris" style="float:right"><a href="/tetris.php">Play Tetris</a></li>
+			<li name="leaderboard" style="float:right"><a href="/leaderboard.php">Leaderboard</a></li>
 		</ul>
 	</body>
 </html>

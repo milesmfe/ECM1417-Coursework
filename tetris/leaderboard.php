cb2d57306bdf3babfd18dbc7d048af7d23acb0f6
Miles Edwards <miles.mfe@gmail.com>
2022-07-21 11:10:35 +0100

    added main div with styling and added bg image

--- tetris/leaderboard.php
+++ tetris/leaderboard.php
@@ -10,5 +10,6 @@
 			<li name="tetris" style="float:right"><a href="tetris.php">Play Tetris</a></li>
 			<li name="leaderboard" style="float:right"><a class="current" href="leaderboard.php">Leaderboard</a></li>
 		</ul>
+		<div id="main"></div>
 	</body>
 </html>

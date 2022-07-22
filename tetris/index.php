2e5733606fba7c3bcec868d14aa315655f2b65d3
Miles Edwards <miles.mfe@gmail.com>
2022-07-22 10:04:31 +0100

    added html body of index

--- tetris/index.php
+++ tetris/index.php
@@ -6,7 +6,7 @@
 
 	<?php
 	
-	$_SERVER["auth"] = true;
+	
 
 	?>
 
@@ -18,12 +18,25 @@
 			<li name="leaderboard" style="float:right"><a href="leaderboard.php">Leaderboard</a></li>
 		</ul>
 		<div id="main">
-			<div class="landing" <?php if ($_SERVER['auth']) { ?>style="display: flex"<?php } else { ?>style="display:none"<?php } ?>>
+			<div class="landing" <?php if ($_SESSION['auth']) { ?>style="display: flex"<?php } else { ?>style="display:none"<?php } ?>>
 				<!-- Logged in -->
 			</div>
-			<div class="landing" <?php if (!$_SERVER['auth']) { ?>style="display: flex"<?php } else { ?>style="display:none"<?php } ?>>
+			<div class="landing" <?php if (!$_SESSION['auth']) { ?>style="display: flex"<?php } else { ?>style="display:none"<?php } ?>>
 				<!-- Logged Out -->
-			</div>
+				<h1>Login</h1>
+				<p>Please login by entering your username and password below.</p>
+				<?php if (!empty($login_error)) { echo '<div class="error">' . $login_error . '</div>'; } ?>
+				<form action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
+					<label for="username">Username</label>
+					<input type="text" name="username" value="<?php echo $username; ?>">
+					<span><?php echo $username_error ?></span>
+					<label for="password">Password</label>
+					<input type="text" name="password" value="<?php echo $password; ?>">
+					<span><?php echo $password_error ?></span>
+					<input type="submit" value="Login">
+					<p>Don't have a user account? <a href="register.php">Register now</a></p>
+				</form>
+			</div>		
 		</div>
 	</body>
 </html>

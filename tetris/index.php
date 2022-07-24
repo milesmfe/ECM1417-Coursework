39cd2cf380cb7e81d70ec2f7f746d8e82c4d40a5
Miles Edwards <miles.mfe@gmail.com>
2022-07-24 12:55:40 +0100

    reverted

--- tetris/index.php
+++ tetris/index.php
@@ -5,85 +5,83 @@
 		<link rel="stylesheet" media="screen" href = "css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php
+	
+		// Include config file
+		require_once "config.php";
 		
-		if (!$_SESSION["auth"]) {
-			// Include config file
-			require_once "config.php";
-			
-			// Define variables and initialize with empty values
-			$username = $password = "";
-			$username_err = $password_err = $login_err = "";
+		// Define variables and initialize with empty values
+		$username = $password = "";
+		$username_err = $password_err = $login_err = "";
 
-			// Processing form data when form is submitted
-			if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		// Processing form data when form is submitted
+		if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		
+			// Check if username is empty
+			if (empty(trim($_POST["username"]))) {
+				$username_err = "Please enter a username.";
+			} else {
+				$username = trim($_POST["username"]);
+			}
 			
-				// Check if username is empty
-				if (empty(trim($_POST["username"]))) {
-					$username_err = "Please enter a username.";
-				} else {
-					$username = trim($_POST["username"]);
-				}
+			// Check if password is empty
+			if (empty(trim($_POST["password"]))) {
+				$password_err = "Please enter your password.";
+			} else {
+				$password = trim($_POST["password"]);
+			}
+			
+			// Validate credentials
+			if (empty($username_err) && empty($password_err)) {
+				// Prepare a select statement
+				$sql = "SELECT username, password FROM Users WHERE username = ?";
 				
-				// Check if password is empty
-				if (empty(trim($_POST["password"]))) {
-					$password_err = "Please enter your password.";
-				} else {
-					$password = trim($_POST["password"]);
-				}
-				
-				// Validate credentials
-				if (empty($username_err) && empty($password_err)) {
-					// Prepare a select statement
-					$sql = "SELECT username, password FROM Users WHERE username = ?";
+				if ($stmt = mysqli_prepare($conn, $sql)) {
+					// Bind variables to the prepared statement as parameters
+					mysqli_stmt_bind_param($stmt, "s", $param_username);
 					
-					if ($stmt = mysqli_prepare($conn, $sql)) {
-						// Bind variables to the prepared statement as parameters
-						mysqli_stmt_bind_param($stmt, "s", $param_username);
+					// Set parameters
+					$param_username = $username;
+					
+					// Attempt to execute the prepared statement
+					if (mysqli_stmt_execute($stmt)) {
+						// Store result
+						mysqli_stmt_store_result($stmt);
 						
-						// Set parameters
-						$param_username = $username;
-						
-						// Attempt to execute the prepared statement
-						if (mysqli_stmt_execute($stmt)) {
-							// Store result
-							mysqli_stmt_store_result($stmt);
-							
-							// Check if username exists, if yes then verify password
-							if (mysqli_stmt_num_rows($stmt) == 1) {                    
-								// Bind result variables
-								mysqli_stmt_bind_result($stmt, $username, $hashed_password);
-								if (mysqli_stmt_fetch($stmt)) {
-									if (password_verify($password, $hashed_password)) {
-										// Password is correct, so start a new session
-										session_start();
-										
-										// Store data in session variables
-										$_SESSION["auth"] = true;
-										$_SESSION["username"] = $username;                            
-										
-										header($_SERVER["PHP_SELF"]);
+						// Check if username exists, if yes then verify password
+						if (mysqli_stmt_num_rows($stmt) == 1) {                    
+							// Bind result variables
+							mysqli_stmt_bind_result($stmt, $username, $hashed_password);
+							if (mysqli_stmt_fetch($stmt)) {
+								if (password_verify($password, $hashed_password)) {
+									// Password is correct, so start a new session
+									session_start();
+									
+									// Store data in session variables
+									$_SESSION["auth"] = true;
+									$_SESSION["username"] = $username;                            
+									
+									header($_SERVER["PHP_SELF"]);
 
-									} else {
-										// Password is not valid, display a generic error message
-										$login_err = "Invalid username or password.";
-									}
+								} else {
+									// Password is not valid, display a generic error message
+									$login_err = "Invalid username or password.";
 								}
-							} else {
-								// Username doesn't exist, display a generic error message
-								$login_err = "Invalid username or password.";
 							}
 						} else {
-							echo "Something went wrong. Please try again later.";
+							// Username doesn't exist, display a generic error message
+							$login_err = "Invalid username or password.";
 						}
-
-						// Close statement
-						mysqli_stmt_close($stmt);
+					} else {
+						echo "Something went wrong. Please try again later.";
 					}
+
+					// Close statement
+					mysqli_stmt_close($stmt);
 				}
-				
-				// Close connection
-				mysqli_close($conn);
 			}
+			
+			// Close connection
+			mysqli_close($conn);
 		}
 
 	?>

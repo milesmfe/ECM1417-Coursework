d3ff5927eff25b7c126d4b0bc047141f5b3887e4
Miles Edwards <miles.mfe@gmail.com>
2022-07-24 12:17:48 +0100

    test

--- tetris/index.php
+++ tetris/index.php
@@ -11,7 +11,7 @@
 		
 		// Define variables and initialize with empty values
 		$username = $password = "";
-		$username_error = $password_error = $login_error = "";
+		$username_error = $password_error = $login_error = "TEEESTING";
 		
 		// Processing form data when form is submitted
 		if ($_SERVER["REQUEST_METHOD"] == "POST") {
@@ -106,7 +106,7 @@
 				<?php if (!empty($login_error)) { echo '<div class="error">' . $login_error . '</div>'; } ?>
 				<form class="login" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 				<input type="text" name="username" placeholder="Username" value="<?php echo $username; ?>">
-				<span class="error"><?php echo "$username_error" ?></span>
+				<span class="error"><?php echo $username_error ?></span>
 				<input type="text" name="password" placeholder="Password" value="<?php echo $password; ?>">
 				<span class="error"><?php echo $password_error ?></span>
 				<input id="login-btn" type="submit" value="Login">

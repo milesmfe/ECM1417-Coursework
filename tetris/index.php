9d44ba1e04ef091f1bbfaff189f8b8442ecb3df0
Miles Edwards <miles.mfe@gmail.com>
2022-07-24 12:20:36 +0100

    errors only show if not empty

--- tetris/index.php
+++ tetris/index.php
@@ -107,8 +107,8 @@
 				<form class="login" action="<?php echo $_SERVER["PHP_SELF"]; ?>" method="post">
 				<input type="text" name="username" placeholder="Username" value="<?php echo $username; ?>">
 				<input type="text" name="password" placeholder="Password" value="<?php echo $password; ?>">
-				<p class="error"><?php echo $username_error ?></p>
-				<p class="error"><?php echo $password_error ?></p>
+				<?php if (!empty($username_error)) { echo '<div class="error">' . $username_error . '</div>'; } ?>
+				<?php if (!empty($password_error)) { echo '<div class="error">' . $password_error . '</div>'; } ?>				
 				<input id="login-btn" type="submit" value="Login">
 				<p id="no-user-msg">Don't have a user account? <a href="register.php">Register now</a></p>
 				</form>

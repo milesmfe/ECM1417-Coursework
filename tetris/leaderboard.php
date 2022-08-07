1a7ca7bf9e09d5a0debdfe1f795fb2b46b6b991e
Miles Edwards <miles.mfe@gmail.com>
2022-08-07 19:33:40 +0100

    fixed css link

--- tetris/leaderboard.php
+++ tetris/leaderboard.php
@@ -2,7 +2,7 @@
 <html>
 	<head>
 		<title>Leaderboard</title>
-		<link rel="stylesheet" href = "css/main.css">
+		<link rel="stylesheet" media="screen" href = "css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 		<?php
 

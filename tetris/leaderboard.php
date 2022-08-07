55066d5a78b887122564cb028a15679843ba844d
Miles Edwards <miles.mfe@gmail.com>
2022-08-07 21:23:48 +0100

    test

--- tetris/leaderboard.php
+++ tetris/leaderboard.php
@@ -13,22 +13,24 @@
 		$player1_name = $player2_name = $player3_name = $player4_name = $player5_name = "";
 		$player1_score = $player2_score = $player3_score = $player4_score = $player5_score = 0;
 
-		$sql = "SELECT Username, Score FROM Scores ORDER BY Score DESC LIMIT 5";
+		$sql = "SELECT Users.UserName, Scores.Score
+				FROM Users
+				LEFT JOIN Scores
+				ON Users.UserName = Scores.UserName
+				WHERE Users.Display = 1
+				ORDER BY Score DESC 
+				LIMIT 5;";
 
 		if ($stmt = mysqli_prepare($conn, $sql)) {
 			// Attempt to execute prepared sql statement
 			if (mysqli_stmt_execute($stmt)) {
 				// Store result
 				mysqli_stmt_store_result($stmt);
-
-				$row = mysqli_stmt_fetch_array($stmt);
-				$player1_name = $row["Username"];
-				$player1_score = $row["Score"];
 			}
 		}
 		mysqli_stmt_close($stmt);
 		mysqli_close($conn);
-		
+
 		?>
 	</head>
 	<body>

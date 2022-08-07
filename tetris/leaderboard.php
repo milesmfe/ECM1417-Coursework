075a4b73af3af43cbb5f1de5c1c979402ffefc26
Miles Edwards <miles.mfe@gmail.com>
2022-08-07 19:27:04 +0100

    lead test

--- tetris/leaderboard.php
+++ tetris/leaderboard.php
@@ -4,6 +4,29 @@
 		<title>Leaderboard</title>
 		<link rel="stylesheet" href = "css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<?php
+
+		// Include config file
+		require_once "config.php";
+		
+		// Define variables and initialise as empty string
+		$player1_name = $player2_name = $player3_name = $player4_name = $player5_name = "";
+		$player1_score = $player2_score = $player3_score = $player4_score = $player5_score = 0;
+
+		$sql = "SELECT Username, Score FROM Scores ORDER BY Score DESC LIMIT 5";
+
+		if ($stmt = mysqli_prepare($conn, $sql)) {
+			// Attempt to execute prepared sql statement
+			if (mysqli_stmt_execute($stmt)) {
+				// Store result
+				mysqli_stmt_store_result($stmt);
+
+				$row = mysqli_stmt_fetch_array($stmt);
+				$player1_name = $row["Username"];
+				$player1_score = $row["Score"];
+			}
+		}
+		?>
 	</head>
 	<body>
 		<ul>
@@ -11,6 +34,13 @@
 			<li name="tetris" style="float:right"><a href="tetris.php">Play Tetris</a></li>
 			<li name="leaderboard" style="float:right"><a class="current" href="leaderboard.php">Leaderboard</a></li>
 		</ul>
-		<div id="main"></div>
+		<div id="main">
+			<div class="landing">
+				<div class="content-box">
+				<?php echo '<div>' . $player1_name . '</div>'; ?>
+				<?php echo '<div>' . $player1_score . '</div>'; ?>
+				</div>
+			</div>
+		</div>
 	</body>
 </html>

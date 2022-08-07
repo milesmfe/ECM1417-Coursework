06c526d18d6a552365da362d4c4e6883ff711c0b
Miles Edwards <miles.mfe@gmail.com>
2022-08-07 19:35:52 +0100

    test

--- tetris/leaderboard.php
+++ tetris/leaderboard.php
@@ -4,29 +4,6 @@
 		<title>Leaderboard</title>
 		<link rel="stylesheet" media="screen" href = "css/main.css">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<?php
-
-		// Include config file
-		require_once "config.php";
-		
-		// Define variables and initialise as empty string
-		$player1_name = $player2_name = $player3_name = $player4_name = $player5_name = "";
-		$player1_score = $player2_score = $player3_score = $player4_score = $player5_score = 0;
-
-		$sql = "SELECT Username, Score FROM Scores ORDER BY Score DESC LIMIT 5";
-
-		if ($stmt = mysqli_prepare($conn, $sql)) {
-			// Attempt to execute prepared sql statement
-			if (mysqli_stmt_execute($stmt)) {
-				// Store result
-				mysqli_stmt_store_result($stmt);
-
-				$row = mysqli_stmt_fetch_array($stmt);
-				$player1_name = $row["Username"];
-				$player1_score = $row["Score"];
-			}
-		}
-		?>
 	</head>
 	<body>
 		<ul>
